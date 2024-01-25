2e838f7c462546382d9cdb0d8eb2fea628379504
patrickchy <chinyeremakapatrick@gmail.com>
2024-01-24 20:44:37 -0600

    my class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module defines the square class
+
+    This class represents a square with no attributes or methods at the moment
+"""
+
+
+class Square:
+    """
+    This is a brief description of the class
+
+    Attributes:
+        No attributes at the moment
+    """
+    pass
